e9e10e461004fd1813a8cfc828852adad8881846
Andrés López <andreslopez01182009@gmail.com>
2026-04-18 18:45:16 -0500

    Nuevas entradas de teclado

--- signal-breaker/scripts/player/states/seeking.gd
+++ signal-breaker/scripts/player/states/seeking.gd
@@ -3,5 +3,10 @@ class_name PlayerSeeking extends EntitySeeking
 
 func physics_update(delta: float) -> void:
 	super(delta)
-	if Input.is_action_just_pressed("attack"): to_state.emit(PlayerAttack)
+	if (
+		Input.is_action_just_pressed("attack_1") or
+		Input.is_action_just_pressed("attack_2") or
+		Input.is_action_just_pressed("attack_3")
+	): to_state.emit(PlayerAttack)
+
 	base_entity.move_direction(base_entity.input_dir)
