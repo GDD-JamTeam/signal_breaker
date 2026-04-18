cbae3e382ce33494b5e4a852c84e12f3b8111525
unknown <solestebano102743@gmail.com>
2026-04-18 10:27:09 -0500

    Separar animacion de tween a su propia funcion

--- signal-breaker/scripts/contador_hits.gd
+++ signal-breaker/scripts/contador_hits.gd
@@ -11,14 +11,15 @@ func _process(_delta: float) -> void:
 		actualizar_contador()
 	
 func actualizar_contador():
+	animacion_pop()
+	hits += 1
+	self.text = str(hits) + "x"
+	
+func animacion_pop():
 	var tween_pop = get_tree().create_tween()
 	var tilt = deg_to_rad(randf_range(-20, 20))
 	var scale_factor = randf_range(3, 3.5)
-	print(rad_to_deg(tilt))
-	#var tween_shrink = get_tree().create_tween()
 	
 	tween_pop.parallel().tween_property(self, "scale", scale*scale_factor, 0.1).set_trans(Tween.TRANS_EXPO)
 	tween_pop.parallel().tween_property(self, "rotation", tilt, 0.1)
 	tween_pop.tween_property(self, "scale", Vector2.ONE, 0.3).set_trans(Tween.TRANS_BACK)
-	hits += 1
-	self.text = str(hits) + "x"
