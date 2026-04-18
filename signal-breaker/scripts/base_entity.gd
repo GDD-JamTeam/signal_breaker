6419ea68ae5cbc7c633fd1dc3eb908afdd7377d6
R2d2cruz <r2d2cruz@gmail.com>
2026-04-17 22:11:10 -0500

    create base entity

---
+++ signal-breaker/scripts/base_entity.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var health: int = 100
+@export var speed: int
+
+
+func _ready() -> void:
+	pass
+
+func move_to(pos: Vector2) -> void:
+	pass
+	
+func move_direction(dir: Vector2) -> void:
+	pass
+	
+func die() -> void:
+	pass
+	
+func get_hurt() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	
+
+	move_and_slide()
