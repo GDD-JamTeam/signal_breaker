2b22573f29df86fe9695e5de3c8fd9e5cb7bf1a7
R2d2cruz <r2d2cruz@gmail.com>
2026-04-18 00:05:21 -0500

    create state machine

--- signal-breaker/scripts/base_enemy.gd
+++ signal-breaker/scripts/base_enemy.gd
@@ -1,8 +1,39 @@
 extends base_entity
 
+# ENUM of states
+enum State {
+	SEEKING,
+	CHARGE,
+	ATTACK,
+	REST
+}
+
 # variables
+@export var charge_time: float = 1.0
+@export var rest_time: float = 1.5
+
 var on_range: bool = false
 var is_alive: bool = true
+var state: State = State.SEEKING
+var timer: float = 0.0
+
+# target
+var player: CharacterBody2D = null
+
+
+func _ready() -> void:
+	var players_group = get_tree().get_nodes_in_group("player")
+	for p in players_group:
+		if p is CharacterBody2D:
+			player = p
+			break
+
+
+func _process(delta: float) -> void:
+	if is_alive and player:
+		behavior_tree(delta)
+
+
 func _on_attack_range_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		var body = area.get_parent()
@@ -15,3 +46,41 @@ func _on_attack_range_area_area_exited(area: Area2D) -> void:
 		var body = area.get_parent()
 		if body is CharacterBody2D:
 			on_range = false
+
+
+func attack() -> void:
+	print("Atacando")
+	change_state(State.REST)
+
+
+func change_state(new_state: State) -> void:
+	timer = 0.0
+	state = new_state
+
+
+func behavior_tree(delta: float) -> void:
+	match state:
+		
+		State.SEEKING:
+			move_to(player.global_position)
+			
+			if on_range:
+				change_state(State.CHARGE)
+		
+		
+		State.CHARGE:
+			timer += delta
+			
+			if timer >= charge_time:
+				change_state(State.ATTACK)
+		
+		
+		State.ATTACK:
+			attack()
+		
+		
+		State.REST:
+			timer += delta
+			
+			if timer >= rest_time:
+				change_state(State.SEEKING)
