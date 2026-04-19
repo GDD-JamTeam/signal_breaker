7bbd8af7f097d135043813a1db96c52e1dc3de11
R2d2cruz <r2d2cruz@gmail.com>
2026-04-19 08:18:44 -0500

    change await for timer

--- signal-breaker/scripts/base_entity/states/charge.gd
+++ signal-breaker/scripts/base_entity/states/charge.gd
@@ -1,11 +1,31 @@
-## Estado de la entidad para cuando acaba un ataque
+## Estado de la entidad para cuando carga antes de atacar
 class_name EntityCharge extends EntityState
+
+var timer: Timer
+
 
 func start() -> void:
 	base_entity.animation_sprite.play(&"idle")
 
-	# Espera a que acabe el tiempo de aturdimiento y se pasa al estado anterior
-	await get_tree().create_timer(base_entity.charge_duration).timeout
+	# Crear timer controlado
+	timer = Timer.new()
+	timer.wait_time = base_entity.charge_duration
+	timer.one_shot = true
+	
+	add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)
+	
+	timer.start()
+
+
+func exit() -> void:
+	if timer:
+		timer.stop()
+		timer.queue_free()
+		timer = null
+
+
+func _on_timer_timeout() -> void:
 	to_state.emit(EntityAttack)
 
 
