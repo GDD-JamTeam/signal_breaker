651ff232187a8aab12985eae58f9b499493d0f5b
unknown <solestebano102743@gmail.com>
2026-04-19 15:05:32 -0500

    Añadir barra de vida funcional

--- signal-breaker/scripts/ui/barra_vida.gd
+++ signal-breaker/scripts/ui/barra_vida.gd
@@ -1,9 +1,18 @@
 extends ProgressBar
 
-func _process(delta: float) -> void:
-	if value != 0:
-		value -= 1
-		print("a")
+var player: Player
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
 	
-	else:
-		print("completo")
+	if player == null:
+		print("[BarraVida] No se encontro al jugador.")
+		return
+	
+	max_value = player.health
+	value = player.get_health()
+	
+	player.get_hurt_signal.connect(_on_player_hurt)
+
+func _on_player_hurt():
+	value = player.get_health()
