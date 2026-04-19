ba90c1d902e6945393924b3572e2cb58ddf1bd30
R2d2cruz <r2d2cruz@gmail.com>
2026-04-18 21:04:20 -0500

    refactor the code for the new paradigm

---
+++ signal-breaker/scripts/base_enemy/base_enemy.gd
@@ -0,0 +1,53 @@
+class_name BaseEnemy extends BaseEntity
+
+
+# variables
+var on_range: bool = false
+var timer: float = 0.0
+
+@export var attack_frames: Array[int]
+@export var hurtbox_damage: float = 100.0
+
+# target
+var player: CharacterBody2D = null
+
+@onready var hurtbox: Area2D = $hurt_box
+
+func get_target():
+	return get_target()
+
+func _ready() -> void:
+	disable_hitboxes()
+	var players_group = get_tree().get_nodes_in_group("player")
+	for p in players_group:
+		if p is CharacterBody2D:
+			player = p
+			break
+	hurtbox.area_entered.connect(apply_damage.bind(hurtbox_damage))
+
+## Activa la hitbox para dar daño
+func enable_hitbox(_index: int) -> void:
+	hurtbox.monitoring = true
+	hurtbox.visible = true
+
+
+## Desactiva las hitbox cuando no se ataca
+func disable_hitboxes() -> void:
+	hurtbox.monitoring = false
+	hurtbox.visible = false
+
+func _on_attack_range_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		var body = area.get_parent()
+		if body is CharacterBody2D:
+			on_range = true
+
+
+func _on_attack_range_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		var body = area.get_parent()
+		if body is CharacterBody2D:
+			on_range = false
+
+func get_attack_frame_range() -> Array:
+	return attack_frames
