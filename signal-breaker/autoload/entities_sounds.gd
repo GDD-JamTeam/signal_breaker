2ee40a35a92d1aa908872050427cdb506f653745
R2d2cruz <r2d2cruz@gmail.com>
2026-05-30 15:37:45 -0500

    create entities sounds script

---
+++ signal-breaker/autoload/entities_sounds.gd
@@ -0,0 +1,15 @@
+# SoundManager.gd
+extends Node
+
+## El diccionario que actúa como librería (Llave: String -> Valor: AudioStream)
+@export var sound_library: Dictionary = {
+	# "tag": preload("file"),
+}
+
+## Función global para obtener un sonido de manera segura
+func get_sound(key: String) -> AudioStream:
+	if sound_library.has(key):
+		return sound_library[key]
+	
+	push_warning("SoundManager: La llave de audio '" + key + "' no existe en la librería.")
+	return null
